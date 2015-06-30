7660d3985390ab032a91851b8c229e315aba67f5
Ubuntu <ubuntu@ip-172-31-1-248.ec2.internal>
2015-06-30 07:08:00 +0000

    going to bed

--- base/__init__.py
+++ base/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/test')
-def test():
-    return 'hello!'
+app.config.from_object('config')
 
 
 from . import views
---
+++ config.py
@@ -0,0 +1,4 @@
+# config for NBA Analytics Flask app
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'a*crazy*key*foo'
